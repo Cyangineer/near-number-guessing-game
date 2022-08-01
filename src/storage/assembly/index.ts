bbcedc17b854d3835a9fa8d1f427f35de6ff6b93
Cyangineer <aarondoriginal@gmail.com>
2022-08-01 12:26:15 -0400

    finished code

---
+++ src/storage/assembly/index.ts
@@ -0,0 +1,64 @@
+import { context, u128, PersistentMap, logging, RNG } from "near-sdk-as"
+
+enum GameState { Created, InProgress, Completed, NotFound }
+
+@nearBindgen
+export class Game {
+    id: u32;
+    gameState: GameState;
+    deposit1: u128;
+    player1: string;
+    player1Guess: u32;
+    compGuess: u32;
+    winner: string;
+    loser: string;
+
+    constructor() {
+        const idValue = new RNG<u32>(1, u32.MAX_VALUE)
+        this.id = idValue.next()
+        this.gameState = GameState.Created
+        this.deposit1 = context.attachedDeposit
+        this.player1 = context.sender
+    }
+}
+
+const games = new PersistentMap<u32, Game>('g')
+
+export function createGame(): u32 {
+    let game = new Game()
+    games.set(game.id, game)
+    logging.log("Game ID: " + game.id.toString())
+    return game.id
+}
+
+export function playGame(gameId: u32, guess: u32): string {
+    const game = games.getSome(gameId)
+    if (game != null) {
+        if (game.gameState == GameState.Created) {
+            game.gameState = GameState.InProgress
+            if (context.sender == game.player1) {
+                game.player1Guess = guess
+                let rng = new RNG<u32>(1, 10)
+                const selectedNum = rng.next()
+                game.compGuess = selectedNum
+
+                if (guess == selectedNum) {
+                    game.gameState = GameState.Completed
+                    game.winner = context.sender
+                    games.set(gameId, game)
+                    logging.log("You Won")
+                    return "won"
+                } else {
+                    game.gameState = GameState.Completed
+                    game.loser = context.sender
+                    games.set(gameId, game)
+                    logging.log("You lost. The number was " + selectedNum.toString())
+                    return "lost"
+                }
+            }
+            return "Wrong player."
+        }
+        return "Game in progress or over."
+    }
+    return "Game not found."
+}
